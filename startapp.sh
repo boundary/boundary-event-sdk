6b7d4ba03477a45e4ab063eeb044fd798ee154a0
jdgwartney <davidg@boundary.com>
2014-03-10 14:02:08 -0700

    Removed the need to run command using sudo

--- startapp.sh
+++ startapp.sh
@@ -4,4 +4,4 @@
 # Execute our Java Application with root priviledges since we 
 # have to bind to a well known port 161 for the SNMP trap handler
 #
-sudo mvn exec:java -Dexec.mainClass="com.boundary.sdk.BoundaryIntegrationApp" 2>&1 > application.log
+mvn exec:java -Dexec.mainClass="com.boundary.sdk.BoundaryIntegrationApp"
