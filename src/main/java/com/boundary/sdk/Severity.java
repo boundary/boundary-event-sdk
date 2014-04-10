7216429c1e65ab3f9ba062f5a25e74e803685b16
jdgwartney <davidg@boundary.com>
2014-04-10 16:28:51 -0700

    Split out of enumerations into their own compilation units

---
+++ src/main/java/com/boundary/sdk/Severity.java
@@ -0,0 +1,19 @@
+package com.boundary.sdk;
+
+/**
+ * Boundary event severity enumeration
+ * 
+ * @author davidg
+ *
+ */
+public enum Severity {
+	
+	INFO,
+	WARN,
+	ERROR,
+	CRITICAL;
+	
+	private Severity() {
+		
+	}
+}
