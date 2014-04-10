dcb8302f56d502d36019d9ca43bbc35a10b0823d
jdgwartney <davidg@boundary.com>
2014-04-10 16:21:38 -0700

    Working version of severity lookup

--- src/main/java/com/boundary/sdk/SyslogToEventProcessor.java
+++ src/main/java/com/boundary/sdk/SyslogToEventProcessor.java
@@ -3,6 +3,9 @@
  */
 package com.boundary.sdk;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
@@ -11,8 +14,7 @@ import org.apache.camel.component.syslog.SyslogMessage;
 import org.apache.camel.component.syslog.SyslogSeverity;
 
 import com.boundary.sdk.RawEvent;
-import com.boundary.sdk.RawEvent.Severity;
-
+import com.boundary.sdk.Severity;
 
 /**
  * @author davidg
@@ -21,83 +23,122 @@ import com.boundary.sdk.RawEvent.Severity;
 public class SyslogToEventProcessor implements Processor {
 
 	private boolean debug = false;
+
 	/**
 	 * 
 	 */
-	
+
 	public SyslogToEventProcessor() {
 		this(false);
 	}
+
 	public SyslogToEventProcessor(boolean debug) {
 		// TODO Auto-generated constructor stub
 	}
-	
-	private final String CAMEL_SYSLOG_HOSTNAME = "CamelSyslogHostname";
-	private final String CAMEL_SYSLOG_SEVERITY = "CamelSyslogSeverity";
 
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		// Get the CamelContext and Message
+
 		CamelContext context = exchange.getContext();
 		Message message = exchange.getIn();
 
-		SyslogMessage sm = (SyslogMessage) message.getBody(SyslogMessage.class);
-		System.out.println("Received syslog message: " + sm);
-		
-		// Create our event so that we can populate with the Syslog data
+		// Extract SyslogMessage from the message body.
+		SyslogMessage syslogMessage = (SyslogMessage) message
+				.getBody(SyslogMessage.class);
+		System.out.println("Received syslog message: " + syslogMessage);
+
+		// Create new event to translate the syslog message to
 		RawEvent event = new RawEvent();
-		event = event.getDefaultEvent();
-		setSeverity(sm,event);
-		event.setTitle(sm.getHostname() + ":" + sm.getTimestamp()); 
-		
+
 		// Delegate to member method call to perform the translation
-		//this.translateSyslogMessage(message, event);
+		syslogMessageToEvent(syslogMessage, event);
+
+		// Set the message body to the RawEvent
 		message.setBody(event, RawEvent.class);
 	}
-	
-	private void translateSyslogMessage(SyslogMessage message, RawEvent event) {
-		
-		// Set severity
-		setSeverity(message,event);
-		
-		
+
+	/**
+	 * Converts a {@link SyslogMessage} to {@link RawEvent}
+	 * 
+	 * @param sm
+	 * @param e
+	 */
+	private void syslogMessageToEvent(SyslogMessage sm, RawEvent e) {
+
+		//
+		// Facility
+		// Hostname
+		// Log Message
+		// Remote Address
+		// Severity
+		// Timestamp
+		// Get the CamelContext and Message
+
+		setEventSeverity(sm, e);
+		e.setTitle(sm.getHostname() + ":" + sm.getTimestamp());
 	}
+
 	/**
 	 * Set the Event from the severity in the Message
+	 * 
 	 * @param m
 	 * @param e
 	 */
-	private void setSeverity(SyslogMessage message, RawEvent event) {
-		
-		switch(message.getSeverity()) {
-		case EMERG:
-			event.setSeverity(Severity.CRITICAL);
-			break;
-		case ALERT:
-			event.setSeverity(Severity.CRITICAL);
-			break;
-		case CRIT:
-			event.setSeverity(Severity.CRITICAL);
-			break;
-		case ERR:
-			event.setSeverity(Severity.ERROR);
-			break;
-		case WARNING:
-			event.setSeverity(Severity.WARN);
-			break;
-		case NOTICE:
-			event.setSeverity(Severity.INFO);
-			break;
-		case INFO:
-			event.setSeverity(Severity.INFO);
-			break;
-		case DEBUG:
-			event.setSeverity(Severity.INFO);
-			break;
-		default:
-			//TODO: How to bail in this case. It should not happen but should still be asserted.
+	private void setEventSeverity(SyslogMessage sm, RawEvent e) {
+		Properties severityMap = getSeverityProperties("syslog.properties");
+//		System.out.println("SEVERITY MAP" + severityMap);
+//		System.out.println(severityMap.getProperty(sm.getSeverity().toString()));
+		String strSeverity = severityMap.getProperty(sm.getSeverity().toString());
+		Severity severity = Severity.valueOf(strSeverity);
+		e.setSeverity(Severity.INFO);
+
+		// switch(message.getSeverity()) {
+		// case EMERG:
+		// event.setSeverity(Severity.CRITICAL);
+		// break;
+		// case ALERT:
+		// event.setSeverity(Severity.CRITICAL);
+		// break;
+		// case CRIT:
+		// event.setSeverity(Severity.CRITICAL);
+		// break;
+		// case ERR:
+		// event.setSeverity(Severity.ERROR);
+		// break;
+		// case WARNING:
+		// event.setSeverity(Severity.WARN);
+		// break;
+		// case NOTICE:
+		// event.setSeverity(Severity.INFO);
+		// break;
+		// case INFO:
+		// event.setSeverity(Severity.INFO);
+		// break;
+		// case DEBUG:
+		// event.setSeverity(Severity.INFO);
+		// break;
+		// default:
+		// //TODO: How to bail in this case. It should not happen but should
+		// still be asserted.
+		// }
+
+	}
+
+	private static Properties severityProperties = null;
+
+	public static Properties getSeverityProperties(String propertyFileName) {
+		if (severityProperties == null) {
+			try {
+				severityProperties = new Properties();
+				severityProperties.load(Thread.currentThread()
+						.getContextClassLoader()
+						.getResourceAsStream(propertyFileName));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-	
+		return severityProperties;
 	}
 
 }
--- src/test/java/com/boundary/sdk/StatusTest.java
+++ src/test/java/com/boundary/sdk/StatusTest.java
@@ -1,14 +1,25 @@
 package com.boundary.sdk;
 
 import static org.junit.Assert.*;
+import com.boundary.sdk.Status;
 
 import org.junit.Test;
 
 public class StatusTest {
-
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testACKNOWLEDGED() {
+		String s = "ACKNOWLEDGED";
+		Status status = Status.fromString(s);
+		assertTrue("Test OK status",status.toString() == s);
 	}
 
+
+	@Test
+	public void testOK() {
+		String s = "OK";
+		Status status = Status.fromString(s);
+		assertTrue("Test OK status",status.toString() == s);
+	}
 }
