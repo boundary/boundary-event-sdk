0c69c74cce3399a1b26b0a0778c58eb0838b7a26
jdgwartney <davidg@boundary.com>
2014-04-10 11:47:30 -0700

    Additional Camel components to convert from SyslogMessage to RawEvent

---
+++ src/main/java/com/boundary/sdk/BaseEvent.java
@@ -0,0 +1,18 @@
+package com.boundary.sdk;
+
+/**
+ * Common properties between RawEvent and Event
+ * 
+ * @author davidg
+ *
+ */
+public class BaseEvent {
+	
+	/**
+	 * Default constructor for BaseEvent
+	 */
+	public BaseEvent() {
+		
+	}
+
+}
