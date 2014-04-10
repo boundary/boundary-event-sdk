927c07fe2c43871a203d6447bc9e7146a024aa83
jdgwartney <davidg@boundary.com>
2014-04-10 16:21:00 -0700

    Break status into its own compilation unit

---
+++ src/test/java/com/boundary/sdk/StatusTest.java
@@ -0,0 +1,14 @@
+package com.boundary.sdk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StatusTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
