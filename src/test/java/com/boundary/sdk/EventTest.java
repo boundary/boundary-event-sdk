a089fe50762b84064e8ae644ecd5ba65442e96ef
jdgwartney <davidg@boundary.com>
2014-04-11 15:37:22 -0700

    Add example camel test

---
+++ src/test/java/com/boundary/sdk/EventTest.java
@@ -0,0 +1,34 @@
+package com.boundary.sdk;
+
+import java.io.File;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.Exchange;
+import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+@SuppressWarnings("deprecation")
+public class EventTest extends CamelSpringTestSupport {
+	protected AbstractXmlApplicationContext createApplicationContext() {
+		return new ClassPathXmlApplicationContext("META-INF/event-test.xml");
+	}
+
+	@Test
+	public void testEventToLog() throws Exception {
+		TestEvent event = new TestEvent("hello");
+		
+		template.sendBody("direct:event-test", event);
+		
+		Thread.sleep(10000);
+
+		File target = new File("target/event-test.log");
+		assertTrue("Log file exists: ", target.exists());
+		String content = context.getTypeConverter().convertTo(String.class,target);
+		assertEquals("TestEvent [name=hello]", content);
+	}
+}
