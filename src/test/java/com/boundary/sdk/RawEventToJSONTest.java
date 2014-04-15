9c4f9de7c8260752f74b59a5e57e224db4366d11
jdgwartney <davidg@boundary.com>
2014-04-14 21:02:12 -0700

    Rename

---
+++ src/test/java/com/boundary/sdk/RawEventToJSONTest.java
@@ -0,0 +1,39 @@
+package com.boundary.sdk;
+
+import java.io.File;
+import java.util.ArrayList;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.CamelSpringTestSupport;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.springframework.context.support.AbstractXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SuppressWarnings("deprecation")
+public class RawEventToJSONTest extends CamelSpringTestSupport {
+	
+    @Produce(uri = "direct:in")
+    private ProducerTemplate producerTemplate;
+
+    @EndpointInject(uri = "mock:out")
+    private MockEndpoint mockOut;
+
+	@Override
+	protected AbstractXmlApplicationContext createApplicationContext() {
+		return new ClassPathXmlApplicationContext("META-INF/boundary-event-to-json.xml");
+	}
+
+	@Test
+	public void testSetTitle() throws Exception {
+		mockOut.setExpectedMessageCount(1);
+		RawEvent event = new RawEvent();
+		
+		event.setTitle("Sample Event");		
+		producerTemplate.sendBody(event);
+		assertMockEndpointsSatisfied();
+	}
+}
