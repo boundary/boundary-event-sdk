4209f999d9a0259b62602f46f4a3dc3b6fd2eb07
jdgwartney <davidg@boundary.com>
2014-04-17 15:06:40 -0700

    Remove dead code from SNMP event processing

--- src/main/java/com/boundary/sdk/SNMPToEventProcessor.java
+++ src/main/java/com/boundary/sdk/SNMPToEventProcessor.java
@@ -9,8 +9,8 @@ import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
-import org.apache.camel.component.syslog.SyslogMessage;
-import org.apache.camel.component.syslog.SyslogSeverity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.boundary.sdk.RawEvent;
 import com.boundary.sdk.Severity;
@@ -21,37 +21,30 @@ import com.boundary.sdk.Severity;
  * 
  */
 public class SNMPToEventProcessor implements Processor {
-
-	private boolean debug = false;
-	/**
-	 * 
-	 */
+	
+    private static final Logger LOG = LoggerFactory.getLogger(SNMPToEventProcessor.class);
 	
 	public SNMPToEventProcessor() {
-		this(false);
-	}
-	public SNMPToEventProcessor(boolean debug) {
-		// TODO Auto-generated constructor stub
 	}
 	
-	private final String CAMEL_SYSLOG_HOSTNAME = "CamelSyslogHostname";
-	private final String CAMEL_SYSLOG_SEVERITY = "CamelSyslogSeverity";
-
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		// Get the CamelContext and Message
-		CamelContext context = exchange.getContext();
 		Message message = exchange.getIn();
+		LOG.debug("class: {}",message.getBody().getClass().toString());
 		
 		// Create our event so that we can populate with the Syslog data
 		RawEvent event = new RawEvent();
-		event = event.getDefaultEvent();
 		Date dt = new java.util.Date();
-		event.setTitle("SNMP TRAP " + dt);
-		event.setSeverity(Severity.CRITICAL);
 		
-		// Delegate to member method call to perform the translation
-		//this.translateSyslogMessage(message, event);
+		event.setTitle("SNMP TRAP " + dt);
+		event.setStatus(Status.OPEN);
+		event.setSeverity(Severity.WARN);
+		event.getSource().setRef("localhost");
+		event.getSource().setType("host");
+//		event.setMessage(message.getBody().toString());
+//		event.putProperty("message",message.getBody().toString());
+		event.putFingerprintField("@title");
+		
 		message.setBody(event, RawEvent.class);
 	}
 }
