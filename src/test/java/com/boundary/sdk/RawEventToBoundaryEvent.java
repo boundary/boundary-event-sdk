48372b771a6691a61bde5e74d3be0f24d76f7f1f
jdgwartney <davidg@boundary.com>
2014-04-17 16:14:59 -0700

    Add test for testing Boundry Event Route

--- src/test/java/com/boundary/sdk/RawEventToBoundaryEvent.java
+++ src/test/java/com/boundary/sdk/RawEventToBoundaryEvent.java
@@ -8,8 +8,9 @@ import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
-
+import static org.junit.Assume.*;
 
 /**
  *
@@ -23,11 +24,15 @@ public class RawEventToBoundaryEvent extends CamelTestSupport {
 	
 	private static final String TEST_IN="direct:in";
 	
+	private static final String BOUNDARY_API_KEY="";
+	private static final String BOUNDARY_ORG_ID="";
+	
 	@Before
     public void setUp() throws Exception {
         super.setUp();
     }
 
+	@Ignore("Requires access to Boundary Server")
     @Override
     protected RouteBuilder[] createRouteBuilders() throws Exception {
     	RouteBuilder[] routes = new RouteBuilder[2];
@@ -35,11 +40,12 @@ public class RawEventToBoundaryEvent extends CamelTestSupport {
     	// Create the Boundary Event Route
 		BoundaryEventRoute boundary = new BoundaryEventRoute();
 		// Configure our properties
+		boundary.setApiKey(BOUNDARY_API_KEY);
+		boundary.setOrgId(BOUNDARY_ORG_ID);
+		boundary.setFromUri(BoundaryEventRoute.DEFAULT_EVENT_TO_URI);
 		
 		routes[0] = boundary;
-		
-		
-		routes[1] = new RouteBuilder(){
+		routes[1] = new RouteBuilder() {
             @Override
             public void configure() throws Exception {
                 from(TEST_IN)
@@ -51,13 +57,14 @@ public class RawEventToBoundaryEvent extends CamelTestSupport {
         return routes;
     }
 
+	@Ignore
     @Test
     public void testDefaultEvent() throws Exception {
+
     	RawEvent event = RawEvent.getDefaultEvent();
     	
     	producerTemplate.sendBody(event);
 
         Thread.sleep(2000);     
     }
-
 }
