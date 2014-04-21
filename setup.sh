84b76eec561071a8da450331533b3b538f93b681
jdgwartney <davidg@boundary.com>
2014-04-21 15:32:06 -0700

    Script refactoring

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
 export BOUNDARY_SDK_HOME=$PWD
+export PATH=$PATH:"$BOUNDARY_SDK_HOME/src/main/scripts"
--- startapp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#mvn exec:java -Dexec.mainClass="com.boundary.sdk.BoundaryIntegrationApp"
-
-# mvn exec:java -Dexec.mainClass="com.boundary.sdk.BoundarySpringSDKApp"
-
-mvn exec:java -Dexec.mainClass="com.boundary.sdk.EventApplication"
