18e323de0efd84f4e4d8ccb6aeb71a8a40d34c93
jdgwartney <davidg@boundary.com>
2014-04-22 16:49:54 -0700

    Working version of scriping to translate an event from foreign even to boundary event

--- src/main/resources/syslog-to-event.js
+++ src/main/resources/syslog-to-event.js
@@ -4,21 +4,21 @@
 
 
 // Extract the body from the request message
-var event = request.headers.event
-event = request.body
-var syslog = request.headers["syslog"];
+var event = request.event
+event = headers.get('event');
+var syslog = headers.get('syslog');
 
 
-event.putTag("red");
-event.putTag("green");
-event.putTag("blue");
-event.putProperty("hello","world");
+event.addTag("red");
+event.addTag("green");
+event.addTag("blue");
+event.addProperty("hello","world");
 var aList = new Array();
 aList.push("yellow");
 aList.push("magenta");
 aList.push("cyan");
 
-event.putProperty("mylist",aList);
+event.addProperty("mylist",aList);
 
 var aSongList = new Array();
 aSongList.push("Red Barchetta");
@@ -26,8 +26,8 @@ aSongList.push("Freewill");
 aSongList.push("La Villa Strangiato");
 
 event.source.ref = "localhost";
-// event.source.type = "host";
-//event.source.putProperty("song_list",aSongList);
+event.source.type = "host";
+event.source.addProperty("song_list",aSongList);
 event
 
 
