17b515c78634be429d5049b1fe0b5e583fb4b968
jdgwartney <davidg@boundary.com>
2014-04-21 18:04:51 -0700

    Added environment variable for location of the MIB repository

--- bsdk-env.sh
+++ bsdk-env.sh
@@ -1,3 +1,4 @@
 export BOUNDARY_SDK_HOME=$PWD
+export BOUNDARY_MIB_REPOSITORY="$BOUNDARY_SDK_HOME/runtime/mibrepository"
 export PATH=$PATH:"$BOUNDARY_SDK_HOME/src/main/scripts"
 alias bsdk='cd $BOUNDARY_SDK_HOME'
