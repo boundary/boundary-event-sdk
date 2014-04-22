6dfd1842ce05ecbe8acf9606770bc7087823a648
jdgwartney <davidg@boundary.com>
2014-04-21 18:03:27 -0700

    Add class to test the SNMPRoute

---
+++ src/test/java/com/boundary/sdk/event/snmp/SNMPRouteTest.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.boundary.sdk.event.snmp;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.boundary.sdk.SNMPRoute;
+
+/**
+ * @author davidg
+ *
+ */
+public class SNMPRouteTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testMibRepository() {
+		String expectedPath = "foobar";
+		SNMPRoute builder = new SNMPRoute();
+		
+		builder.setMibRepository(expectedPath);
+		assertEquals("Check license",expectedPath,builder.getMibRepository());
+	}
+
+	@Test
+	public void testLicense() {
+		String expectedLicense = "foobar";
+		SNMPRoute builder = new SNMPRoute();
+		
+		builder.setLicense(expectedLicense);
+		assertEquals("Check license",expectedLicense,builder.getLicense());
+	}
+
+}
