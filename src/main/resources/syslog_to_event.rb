0d149f49d0870358b6f2ced9c5b0ef8a86624171
jdgwartney <davidg@boundary.com>
2014-04-23 15:55:17 -0700

    Test to validat the translation of Syslog Messages to RawEvents

---
+++ src/main/resources/syslog_to_event.rb
@@ -0,0 +1,31 @@
+
+
+
+##Extract the body from the request message
+
+event = headers.get('event')
+syslog = headers.get('syslog')
+
+
+# event.addTag("red")
+# event.addTag("green")
+# event.addTag("blue")
+# event.addProperty("hello","world")
+# aList = []
+# aList.push("yellow")
+# aList.push("magenta")
+# aList.push("cyan")
+# 
+# event.addProperty("mylist",aList);
+# 
+# aSongList = []
+# aSongList.push("Red Barchetta")
+# aSongList.push("Freewill")
+# aSongList.push("La Villa Strangiato")
+# 
+# event.source.ref = "localhost"
+# event.source.type = "host"
+# event.source.addProperty("song_list",aSongList)
+
+
+
--- src/test/java/com/boundary/sdk/SyslogToEventScriptingTest.java
+++ src/test/java/com/boundary/sdk/SyslogToEventScriptingTest.java
@@ -5,48 +5,107 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.component.syslog.SyslogMessage;
 import org.apache.camel.test.junit4.CamelSpringTestSupport;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.AbstractXmlApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * Smoke test to test mapping of a {@link SyslogMessage} to a {@link RawEvent}
+ * 
+ * @author davidg
+ *
+ */
 @SuppressWarnings("deprecation")
 public class SyslogToEventScriptingTest extends CamelSpringTestSupport {
 	
-	private static Logger LOG = LoggerFactory.getLogger(SyslogToEventScriptingTest.class);
+	SyslogMessage sm;
+	RawEvent e;
+	HashMap<String,Object> headers;
 	
-	private static final String SYSLOG_IN = "direct:syslog-in";
-	private static final String EVENT_OUT = "mock:event-out";
+	//private static Logger LOG = LoggerFactory.getLogger(SyslogToEventScriptingTest.class);
+	
+	private static final String SYSLOG_JS_IN = "direct:syslog-js-in";
+	private static final String EVENT_JS_OUT = "mock:event-js-out";
+	
+	private static final String SYSLOG_PY_IN = "direct:syslog-py-in";
+	private static final String EVENT_PY_OUT = "mock:event-py-out";
+
+	private static final String SYSLOG_RB_IN = "direct:syslog-rb-in";
+	private static final String EVENT_RB_OUT = "mock:event-rb-out";
 
 	protected AbstractXmlApplicationContext createApplicationContext() {
 		return new ClassPathXmlApplicationContext("META-INF/syslog-to-event-scripting.xml");
 	}
-
-	@Test
-	public void testEventToLog() throws Exception {
-		MockEndpoint eventOut = getMockEndpoint(EVENT_OUT);
-		eventOut.setExpectedMessageCount(1);
-		SyslogMessage sm = new SyslogMessage();
-		RawEvent event = new RawEvent();
-		
-		Map<String,Object> headers = new HashMap<String,Object>();
-		
+	
+	@Before
+	public void setUp() throws Exception {
+		super.setUp();
+		sm = new SyslogMessage();
+		e = new RawEvent();
+		headers = new HashMap<String,Object>();
 		headers.put("syslog", sm);
-		headers.put("event",event);
-		
-		template.sendBodyAndHeaders(SYSLOG_IN, event,headers);
-		
-		List<Exchange> exchanges = eventOut.getExchanges();
-		
+		headers.put("event",e);
+	}
+	/**
+	 * Utility
+	 * @param in
+	 * @param out
+	 * @throws InterruptedException
+	 */
+	private void getEvent(String inUrl, String outUrl) throws InterruptedException {
+		Endpoint in = getMandatoryEndpoint(inUrl);
+		assertNotNull(in);
+		MockEndpoint out = getMockEndpoint(outUrl);
+		assertNotNull(out);
+
+		out.setExpectedMessageCount(1);
+
+		template.sendBodyAndHeaders(in,e,headers);
+
+		List<Exchange> exchanges = out.getExchanges();
 		RawEvent newEvent = exchanges.get(0).getIn().getBody(RawEvent.class);
 		
 		System.out.println(newEvent);
 		
-		eventOut.assertIsSatisfied(); 
+		out.assertIsSatisfied(); 
+	}
+	
+	/**
+	 * 
+	 * Java Script
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testSysLogToEventJavaScript() throws Exception {
+		getEvent(SYSLOG_JS_IN,EVENT_JS_OUT);
+	}
+	
+	/**
+	 * Python
+	 * 
+	 * @throws InterruptedException
+	 */
+	//@Test
+	public void testSysLogToEventPython() throws InterruptedException {
+		getEvent(SYSLOG_PY_IN,EVENT_PY_OUT);
+	}
+	
+	/**
+	 * Ruby
+	 * 
+	 * @throws InterruptedException
+	 */
+	//@Test
+	public void testSysLogToEventRuby() throws InterruptedException {
+		getEvent(SYSLOG_RB_IN,EVENT_RB_OUT);
 	}
 }
