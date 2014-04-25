0a27f657bbbfd5547da082788457f7a7b71cba1b
jdgwartney <davidg@boundary.com>
2014-04-24 21:51:40 -0700

    Utility class for methods scripting

---
+++ src/test/java/com/boundary/sdk/ScriptUtils.java
@@ -0,0 +1,40 @@
+package com.boundary.sdk;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.net.URL;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.boundary.sdk.event.BaseScriptTest;
+
+public class ScriptUtils {
+	
+	private static Logger LOG = LoggerFactory.getLogger(ScriptUtils.class);
+
+	public ScriptUtils() {
+	}
+	
+	/**
+	 * Finds a script to be tested from 
+	 * @param scriptName
+	 * @return {@link File}
+	 */
+	static public FileReader getFile(String scriptPath) {
+		ClassLoader loader = Thread.currentThread().getContextClassLoader();
+
+		URL url = loader.getResource(scriptPath);
+		assert(url == null);
+		File f = new File(url.getFile());
+		FileReader reader = null;
+		try {
+			reader = new FileReader(f);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			LOG.error(e.getMessage());
+		}
+		return reader;
+	}
+}
