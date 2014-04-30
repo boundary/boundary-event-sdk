6e4343be8bfe95b8690ce020c9bd65da71416eed
jdgwartney <davidg@boundary.com>
2014-04-29 22:44:27 -0700

    Add environment variable for version of the SDK

--- bsdk-env.sh
+++ bsdk-env.sh
@@ -1,4 +1,5 @@
 export BOUNDARY_SDK_HOME=$PWD
+export BOUNDARY_SDK_VERSION=0.00.03
 export BOUNDARY_MIB_REPOSITORY="$BOUNDARY_SDK_HOME/runtime/mibrepository"
 export PATH=$PATH:"$BOUNDARY_SDK_HOME/src/main/scripts"
 alias bsdk='cd $BOUNDARY_SDK_HOME'
