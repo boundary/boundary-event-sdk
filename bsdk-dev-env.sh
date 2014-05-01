0807d3712f2f132f469bf097268c0ce07fb58c60
jdgwartney <davidg@boundary.com>
2014-05-01 11:33:52 -0700

    Better the script to be able to be sourced from any directory location ; pulls version information from the pom.xml

--- bsdk-dev-env.sh
+++ bsdk-dev-env.sh
@@ -1,5 +1,7 @@
+pushd $(dirname ${BASH_SOURCE[0]}) > /dev/null
 export BOUNDARY_SDK_HOME=$PWD
-export BOUNDARY_SDK_VERSION=0.00.03
+popd > /dev/null
+export BOUNDARY_SDK_VERSION=$(egrep "<version>(.*)</version>" "$BOUNDARY_SDK_HOME/pom.xml" | head -1 | sed '-e s/<version>//' -e 's/<\/version>//' -e 's/[[:blank:]]//')
 export BOUNDARY_MIB_REPOSITORY="$BOUNDARY_SDK_HOME/runtime/mibrepository"
 export PATH=$PATH:"$BOUNDARY_SDK_HOME/src/main/scripts"
 alias bsdk='cd $BOUNDARY_SDK_HOME'
