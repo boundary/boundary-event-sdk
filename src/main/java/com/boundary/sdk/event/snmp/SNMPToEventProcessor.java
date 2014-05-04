5811d88836dd120e1b6344dd005f7ea3054622a2
jdgwartney <davidg@boundary.com>
2014-05-04 14:57:44 -0700

    Update log4j configuration for new package namin

--- src/main/java/com/boundary/sdk/event/snmp/SNMPToEventProcessor.java
+++ src/main/java/com/boundary/sdk/event/snmp/SNMPToEventProcessor.java
@@ -13,6 +13,7 @@ import org.apache.camel.component.snmp.SnmpMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.snmp4j.PDU;
+import org.snmp4j.PDUv1;
 import org.snmp4j.smi.VariableBinding;
 
 import com.boundary.sdk.event.RawEvent;
@@ -71,13 +72,25 @@ public class SNMPToEventProcessor implements Processor {
 		SnmpMessage snmpMessage = message.getBody(SnmpMessage.class);
 		PDU pdu = snmpMessage.getSnmpMessage();
 		
-		LOG.debug("PDU: {}",pdu);
+		LOG.debug("PDU: " + pdu);
 		
 		// Create our event so that we can populate with the Syslog data
 		RawEvent event = new RawEvent();
 		
 		// Get the type of trap received either: TRAP (v2) or TRAPV1 (v1)
-		String trapType = PDU.getTypeString(pdu.getType());
+		String trapType = PDU.getTypeString(pdu.getType()) == "TRAP" ? "v2c" : "v1";
+		
+        // Extract SNMPv1 specific variables
+        if (pdu.getType() == PDU.V1TRAP) {
+            PDUv1 v1pdu = (PDUv1) pdu;
+            event.addProperty("enterprise", v1pdu.getEnterprise().toString());
+            event.addFingerprintField("enterprise");
+            event.addProperty("agent-addr", v1pdu.getAgentAddress().toString());
+            event.addProperty("generic-trap", Integer.toString(v1pdu.getGenericTrap()));
+            event.addProperty("specific-trap", Integer.toString(v1pdu.getSpecificTrap()));
+            event.addProperty("time-stamp", Long.toString(v1pdu.getTimestamp()));
+        }
+
 		
 		// Get the variable bindings from the trap and create properties in the event
 		Vector<? extends VariableBinding> varBinds = pdu.getVariableBindings();
@@ -88,12 +101,14 @@ public class SNMPToEventProcessor implements Processor {
 		event.addProperty("error_status", pdu.getErrorStatusText());
 		
 		// Address is in the form of X.X.X.X/port if IPV4
-		String hostname = snmpMessage.getHeader("peerAddress").toString();
+		String [] s = snmpMessage.getHeader("peerAddress").toString().split("/");
+		String hostname = s[0];
+		event.addProperty("hostname",hostname);
+		event.addFingerprintField("hostname");
 
-		//TBD, Set title using the trap type and hostname
 		event.setTitle(trapType + " from " + hostname);
 		
-		event.addProperty("type", trapType == "TRAP" ? "v2c" : "v1");
+		event.addProperty("type", trapType);
 		
 		//TBD, What should the message field be set to by default??
 		event.setMessage(pdu.getErrorStatusText());
@@ -107,7 +122,7 @@ public class SNMPToEventProcessor implements Processor {
 		event.getSource().setRef(hostname);
 		event.getSource().setType("host");
 		
-		LOG.debug("RawEvent: {}",event);
+		LOG.debug("RawEvent: " + event);
 		
 		// Assign the RawEvent to the body
 		message.setBody(event, RawEvent.class);
