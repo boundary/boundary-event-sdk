aa1a41b4b6691e16d0ae2fc25d3af548ed0a3fb8
jdgwartney <davidg@boundary.com>
2014-05-06 21:12:49 -0700

    Change logging parameters

--- src/main/java/com/boundary/sdk/event/BoundaryEventRouteBuilder.java
+++ src/main/java/com/boundary/sdk/event/BoundaryEventRouteBuilder.java
@@ -112,9 +112,9 @@ public class BoundaryEventRouteBuilder extends BoundaryRouteBuilder {
 			.setHeader(Exchange.ACCEPT_CONTENT_TYPE, constant("application/json"))
 			.setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
 			.setHeader(Exchange.HTTP_METHOD, constant("POST"))
-			.to("log:com.boundary.sdk.event.BoundaryEventRoute?level=DEBUG&groupInterval=60000&groupDelay=60000&groupActiveOnly=false")
+			.to("log:" + BoundaryEventRouteBuilder.class + "?level=DEBUG&groupInterval=60000&groupDelay=60000&groupActiveOnly=false")
 			.to(url.toString())
-			.to("log:com.boundary.sdk.event.BoundaryEventRoute?level=INFO&showHeaders=true&multiline=true")
+			.to("log:" + BoundaryEventRouteBuilder.class + "?level=INFO&showHeaders=true&multiline=true")
 			;
 	}
 }
