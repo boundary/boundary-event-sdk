a7cc4f1fbc1fa7e3910ea7745002ee7320b5d64e
jdgwartney <davidg@boundary.com>
2014-05-13 15:34:16 -0700

    Define command line options
    Former-commit-id: 549e6fe398639c7145477fdfcb5bc57c849bf5ac

--- src/main/java/com/boundary/sdk/event/EventCLI.java
+++ src/main/java/com/boundary/sdk/event/EventCLI.java
@@ -16,6 +16,19 @@ public class EventCLI {
 	private Option apiKey;
 	private Option command;
 	private Option orgId;
+	
+	private Option optionCreatedAt;
+	private Option optionFingerprintFields;
+	private Option optionMessage;
+//	private Option optionOrganizationId;
+	private Option optionProperties;
+	private Option optionReceivedAt;
+	private Option optionSender;
+	private Option optionSeverity;
+	private Option optionSource;
+	private Option optionStatus;
+	private Option optionTags;
+	private Option optionTitle;
 
 	public EventCLI() {
 		// create Options object
@@ -24,28 +37,84 @@ public class EventCLI {
 
 	@SuppressWarnings("static-access")
 	private void handleCommandlandArguments(String[] args) {
-		apiHost = OptionBuilder.withArgName("api_host").hasArg().create("b");
-		command = OptionBuilder.withArgName("command").hasArg().create("c");
-		apiKey = OptionBuilder.withArgName("api_key").hasArg().create("a");
+		apiHost = OptionBuilder.withArgName("api_host").hasArg()
+				.withDescription("Boundary API Host. defaults to api.boundary.com").create("b");
+		command = OptionBuilder.withArgName("command").hasArg()
+				.withDescription("One of CREATE, UPDATE, OR DELETE").create("c");
+		apiKey = OptionBuilder.withArgName("api_key").hasArg()
+				.withDescription("Boundary API Key").create("a");
 		apiKey.setLongOpt("api-key");
 		orgId = OptionBuilder.withArgName("org_id").hasArg()
 				.withDescription("Boundary organization Id")
 				.withLongOpt("org-id").create("o");
+		
+		optionCreatedAt = OptionBuilder.withArgName("yyyy-mm-dd HH-MM-SS").hasArg()
+				.withDescription("Date and time of event creation")
+				.withLongOpt("created-at").create("z");
+		
+		optionFingerprintFields = OptionBuilder.withArgName("field-name").hasArg()
+				.withDescription("The fields of the event used to calculate the event fingerprint.")
+				.withLongOpt("fingerprint-fields").create("f");
+		
+		optionMessage = OptionBuilder.withArgName("message").hasArg()
+				.withDescription("Additional description of the event")
+				.withLongOpt("message").create("m");
+		
+		optionProperties = OptionBuilder.withArgName("properties").hasArg()
+				.withDescription("Properties for the event.")
+				.withLongOpt("properties").create("p");
+		
+		optionReceivedAt = OptionBuilder.withArgName("yyyy-mm-dd HH-MM-SS").hasArg()
+				.withDescription("The timestamp the event was received.")
+				.withLongOpt("received-at").create("r");
+		
+		optionSender = OptionBuilder.withArgName("ref:type[:name]").hasArg()
+				.withDescription("Optional information about the sender of the event.")
+				.withLongOpt("sender").create("x");
+		
+		optionSeverity = OptionBuilder.withArgName("severity").hasArg()
+				.withDescription("Severity of the event which is one of INFO, WARN, ERROR, CRITICAL. Default is INFO.")
+				.withLongOpt("severity").create("y");
+
+		optionSource = OptionBuilder.withArgName("ref:type[:name]").hasArg()
+				.withDescription("The source of the event. The source is typically the hostname or ip address of the system this event refers to.")
+				.withLongOpt("status").create("u");
+		
+		optionStatus = OptionBuilder.withArgName("status").hasArg()
+				.withDescription("One of OPEN, CLOSED, ACKNOWLEDGED, or OK.")
+				.withLongOpt("status").create("w");
+		
+		optionTags = OptionBuilder.withArgName("tag1[:tag2][:tag3][:...]").hasArg()
+				.withDescription("Tags used to provide a classification for events.")
+				.withLongOpt("tags").create("t");
+		
+		optionTitle = OptionBuilder
+					.withDescription("Title of the event")
+					.withLongOpt("title").create("n");
 
 		try {
-
-			// add t option
-			options.addOption("t", false, "display current time");
-			// add c option
-			options.addOption("c", true, "country code");
 			// add h option
-			options.addOption("h", false, "show help");
+			options.addOption("h", false, "Show help");
 
 			// add v option
 			options.addOption(apiHost);
 			options.addOption(apiKey);
 			options.addOption(orgId);
 			options.addOption(command);
+			
+			// Event options
+			options.addOption(optionCreatedAt);
+			options.addOption(optionFingerprintFields);
+			options.addOption(optionMessage);
+//			options.addOption(optionOrganizationId);
+			options.addOption(optionProperties);
+			options.addOption(optionReceivedAt);
+			options.addOption(optionSender);
+			options.addOption(optionSeverity);
+			options.addOption(optionSource);
+			options.addOption(optionStatus);
+			options.addOption(optionTitle);
+			options.addOption(optionTags);
 
 			CommandLineParser parser = new BasicParser();
 			CommandLine cmd = parser.parse(options, args);
