e0378dc0f0d2186ebd53465c64428774523829b2
jdgwartney <davidg@boundary.com>
2014-05-13 14:29:30 -0700

    Add the concurrent consumers options and set to 10
    Former-commit-id: e8b7c63ee9ce167c7d685f25c604a10c60e6e4b5

--- src/main/java/com/boundary/sdk/event/snmp/SNMPToEventProcessor.java
+++ src/main/java/com/boundary/sdk/event/snmp/SNMPToEventProcessor.java
@@ -4,6 +4,7 @@
 package com.boundary.sdk.event.snmp;
 
 import java.io.File;
+import java.util.Date;
 import java.util.Vector;
 
 import org.apache.camel.Exchange;
@@ -35,6 +36,10 @@ public class SNMPToEventProcessor implements Processor {
     private static final Logger LOG = LoggerFactory.getLogger(SNMPToEventProcessor.class);
     
     SmiSupport smi;
+    
+    private final static String HOSTNAME_PROPERTY_NAME = "hostname";
+    private final static String SENDER_REF = "Boundary Event SDK";
+    private final static String SENDER_TYPE = "Boundary Event SDK";
 	
     /**
      * 
@@ -67,39 +72,82 @@ public class SNMPToEventProcessor implements Processor {
 		return smi.getLicense();
 	}
 	
-	public void processV2Traps(PDU pdu, RawEvent event) {
+	/**
+	 * Converts an {@link SnmpMessage}/{@link PDUv1} into a {@link RawEvent}
+	 * 
+	 * @param message - Contains a {@link SnmpMessage}
+	 * @param pdu - Contains a {@PDUv1} with the actual trap contents
+	 * @param event - {@link RawEvent} to be populated.
+	 */
+	public void processV1Trap(SnmpMessage message, PDU pdu, RawEvent event) {
+        PDUv1 v1pdu = (PDUv1) pdu;
+		String hostname = getPeerAddress(message);
+
+		// CREATED_AT,
+        // event.setCreatedAt(v1pdu.getTimestamp());
 		
+		// FINGERPRINT_FIELDS
+		event.addFingerprintField(HOSTNAME_PROPERTY_NAME);
+        event.addFingerprintField("enterprise");
+		
+		// MESSAGE - TBD: Set based on the content of the trap?
+		
+		// ORGANIZATION_ID - TBD: Override based on content of trap?
+		
+		// PROPERTIES
+		event.addProperty(HOSTNAME_PROPERTY_NAME, hostname);
+        event.addProperty("enterprise", v1pdu.getEnterprise().toString());
+        event.addProperty("agent-addr", v1pdu.getAgentAddress().toString());
+        event.addProperty("generic-trap", Integer.toString(v1pdu.getGenericTrap()));
+        event.addProperty("trap",Integer.toString(v1pdu.getSpecificTrap()));
+        event.addProperty("time-stamp", Long.toString(v1pdu.getTimestamp()));
+
+		// RECEIVED_AT - Default to value set by Boundary
+		
+		// SENDER - Defaults to the Boundary Event SDK
+		event.getSender().setRef(SENDER_REF).setType(SENDER_TYPE);
+		
+		// SEVERITY - TBD: Set severity based on content of trap?
+		event.setSeverity(Severity.WARN);
+		
+		// SOURCE
+		event.getSource().setRef(hostname).setType("hostname");
+		
+		//STATUS - TBD: Depends on trap fields?
+		event.setStatus(Status.OPEN);
+		
+		// TAGS
+		event.addTag(hostname);
+        
+        // TITLE
+		event.setTitle(Integer.toString(v1pdu.getSpecificTrap()) + " trap received from " + hostname);
 	}
 	
-	@Override
-	public void process(Exchange exchange) throws Exception {
-		String specificTrap="";
-		
-		Message message = exchange.getIn();
-		SnmpMessage snmpMessage = message.getBody(SnmpMessage.class);
-		PDU pdu = snmpMessage.getSnmpMessage();
-		
-		LOG.debug("PDU: " + pdu);
-		
-		// Create our event so that we can populate with the Syslog data
-		RawEvent event = new RawEvent();
-		
-		// Get the type of trap received either: TRAP (v2) or TRAPV1 (v1)
-		String trapVersion = PDU.getTypeString(pdu.getType()) == "TRAP" ? "v2c" : "v1";
-		event.addProperty("trapVersion", trapVersion);
-		
-        // Extract SNMPv1 specific variables
-        if (pdu.getType() == PDU.V1TRAP) {
-            PDUv1 v1pdu = (PDUv1) pdu;
-            event.addProperty("enterprise", v1pdu.getEnterprise().toString());
-            event.addFingerprintField("enterprise");
-            event.addProperty("agent-addr", v1pdu.getAgentAddress().toString());
-            event.addProperty("generic-trap", Integer.toString(v1pdu.getGenericTrap()));
-            specificTrap = Integer.toString(v1pdu.getSpecificTrap());
-            event.addProperty("trap",specificTrap);
-            event.addProperty("time-stamp", Long.toString(v1pdu.getTimestamp()));
-        }
+	/**
+	 * Converts an {@link SnmpMessage}/{@link PDU} into a {@link RawEvent}
+	 * 
+	 * @param message - Contains a {@link SnmpMessage}
+	 * @param pdu - Contains a {@PDU} with the actual trap contents
+	 * @param event - {@link RawEvent} to be populated.
+	 */
+	public void processV2Trap(SnmpMessage message, PDU pdu, RawEvent event) {
+		String specificTrap = "";
+		String hostname = getPeerAddress(message);
 
+		
+		// CREATED_AT - TBD: Get from PDU
+		event.setCreatedAt(new Date());
+
+		// FINGERPRINT_FIELDS
+		event.addFingerprintField(HOSTNAME_PROPERTY_NAME);
+		
+		// MESSAGE - TBD: Handled based on the content of the PDU?
+		
+		// ORGANIZATION_ID - TBD: Override based on content of the PDU?
+		
+		// PROPERTIES
+		event.addProperty(HOSTNAME_PROPERTY_NAME, hostname);
+		event.addProperty("error_status", pdu.getErrorStatusText());
 		
 		// Get the variable bindings from the trap and create properties in the event
 		Vector<? extends VariableBinding> varBinds = pdu.getVariableBindings();
@@ -117,29 +165,65 @@ public class SNMPToEventProcessor implements Processor {
 			}
 		}
 		
-		event.addFingerprintField("trap");
+		// RECEIVED_AT - Default to value set by Boundary
 		
-		event.addProperty("error_status", pdu.getErrorStatusText());
-		
-		// Address is in the form of X.X.X.X/port if IPV4
-		String [] s = snmpMessage.getHeader("peerAddress").toString().split("/");
-		String hostname = s[0];
-		event.addProperty("hostname",hostname);
-		event.addFingerprintField("hostname");
+		// SENDER
+		event.getSender().setRef(SENDER_REF).setType(SENDER_TYPE);
 
-		event.setTitle(specificTrap + " trap received from " + hostname);
-				
-		//TBD, set the severity based on content of the trap
+		// SEVERITY - TBD: set the severity based on content of the trap
 		event.setSeverity(Severity.WARN);
+
+		// SOURCE
+		event.getSource().setRef(hostname).setType("hostname");
 		
-		//TBD Set status based on severity??
+		//STATUS - TBD: Set status based on severity??
 		event.setStatus(Status.OPEN);
 
-		event.getSource().setRef(hostname);
-		event.getSource().setType("host");
+		// TAGS
+		event.addTag(hostname);
 		
+		// TITLE
+		event.setTitle(specificTrap + " trap received from " + hostname);
+	}
+	
+	/**
+	 * Help function to get the peer address from an {@link SnmpMessage}.
+	 * 
+	 * @param message
+	 * @return String
+	 */
+	private String getPeerAddress(SnmpMessage message) {
+		String [] s = message.getHeader("peerAddress").toString().split("/");
+		String address = s[0];
+		return address;
+	}
+	
+	/**
+	 * Handles the translation of a {@link SnmpMessage} to {@link RawEvent}
+	 * 
+	 */
+	@Override
+	public void process(Exchange exchange) throws Exception {
+		// Extract the SnmpMessage and PDU instances from the Camel Exchange
+		Message message = exchange.getIn();
+		SnmpMessage snmpMessage = message.getBody(SnmpMessage.class);
+		PDU pdu = snmpMessage.getSnmpMessage();
+
+		// Create our event so that we can populate with SNMP data.
+		RawEvent event = new RawEvent();
+
+		// Dispatch based on PDU type of the trap
+		// TBD: Handling of v3 PDU traps
+		if (pdu.getType() == PDU.V1TRAP) {
+			processV1Trap(snmpMessage, pdu, event);
+		}
+		else {
+			processV2Trap(snmpMessage, pdu, event);
+		}
+
+		// Dump out the contents of our event for debugging purposes
 		LOG.debug("RawEvent: " + event);
-		
+
 		// Assign the RawEvent to the body
 		message.setBody(event, RawEvent.class);
 	}
