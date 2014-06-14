6cb64ff35432937f299ee456bc1b4f5d746e91b4
jdgwartney <davidg@boundary.com>
2014-06-14 08:58:42 -0700

    Bean to handle translation of a Service Check to a RawEvent
    Former-commit-id: 0512f0f9eee1ea3d27da7ee59b0742417efa7c5e

---
+++ src/main/java/com/boundary/sdk/event/service/ServiceCheckToRawEvent.java
@@ -0,0 +1,26 @@
+package com.boundary.sdk.event.service;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
+
+import com.boundary.camel.component.ping.PingConfiguration;
+import com.boundary.camel.component.port.PortConfiguration;
+import com.boundary.sdk.event.RawEvent;
+
+import static com.boundary.sdk.event.service.ServiceCheckPropertyNames.*;
+
+public class ServiceCheckToRawEvent {
+
+	/**
+	 * 
+	 * @param exchange
+	 */
+	public void toEvent(Exchange exchange) {
+		Message message = exchange.getIn();
+		ServiceCheckRequest request = message.getHeader(SERVICE_CHECK_REQUEST_INSTANCE,ServiceCheckRequest.class);
+		ServiceCheckResults results = message.getBody(ServiceCheckResults.class);
+		RawEvent event = new RawEvent();
+
+	}
+}
+
