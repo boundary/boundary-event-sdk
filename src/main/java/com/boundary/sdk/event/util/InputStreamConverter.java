65b286664a63f1eb481f6a8d92e5cb016b5b9212
jdgwartney <davidg@boundary.com>
2014-06-20 12:51:37 -0700

    Add converstion to String
    Former-commit-id: 7b855d5a71b8078f6485a1aa82d43c60a3204137

--- src/main/java/com/boundary/sdk/event/util/InputStreamConverter.java
+++ src/main/java/com/boundary/sdk/event/util/InputStreamConverter.java
@@ -38,5 +38,13 @@ public final class InputStreamConverter {
 		
 		return lines;
 	}
+	
+	@Converter static String toString(List<String> list) {
+		StringBuffer sb = new StringBuffer();
+		for (String s :list) {
+			sb.append(s);
+		}
+		return sb.toString();
+	}
 
 }
