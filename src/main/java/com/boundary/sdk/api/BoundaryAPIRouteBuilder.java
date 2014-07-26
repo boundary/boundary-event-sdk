b9e99e704a65d64f0b26a514d2d0557a0c7960f4
jdgwartney <davidg@boundary.com>
2014-07-25 19:27:28 -0700

    Fixed javadoc compilation error

--- src/main/java/com/boundary/sdk/api/BoundaryAPIRouteBuilder.java
+++ src/main/java/com/boundary/sdk/api/BoundaryAPIRouteBuilder.java
@@ -1,11 +1,23 @@
+// Copyright 2014 Boundary, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.boundary.sdk.api;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.http.AuthMethod;
 import org.apache.camel.component.http.HttpComponent;
 import org.apache.camel.component.http.HttpConfiguration;
-import org.apache.camel.model.dataformat.JsonLibrary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -206,7 +218,7 @@ public abstract class BoundaryAPIRouteBuilder extends BoundaryRouteBuilder {
 	
 	/**
 	 * Set path
-	 * @param path
+	 * @param path {@link String}
 	 */
 	public void setPath(String path) {
 		this.path  = path;
