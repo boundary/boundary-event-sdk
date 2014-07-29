f3954673e8acf91ad664681e690a6ec6f5c7ad94
jdgwartney <davidg@boundary.com>
2014-07-29 12:35:43 -0700

    Exec routes and tests

--- src/test/java/com/boundary/sdk/event/exec/ExecTest.java
+++ src/test/java/com/boundary/sdk/event/exec/ExecTest.java
@@ -13,15 +13,15 @@
 // limitations under the License.
 package com.boundary.sdk.event.exec;
 
-import static com.boundary.sdk.event.notification.NotificationTestUtil.*;
+import static org.apache.camel.component.exec.ExecBinding.EXEC_COMMAND_ARGS;
+import static org.apache.camel.component.exec.ExecBinding.EXEC_COMMAND_EXECUTABLE;
 
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 import org.apache.camel.EndpointInject;
 import org.apache.camel.Exchange;
@@ -32,19 +32,25 @@ import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringTestSupport;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.boundary.sdk.event.notification.Notification;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import static org.apache.camel.component.exec.ExecBinding.*;
-
 public class ExecTest extends CamelSpringTestSupport {
+	
+	private static Logger LOG = LoggerFactory.getLogger(ExecTest.class);
+	
+	private Map<String,Object> headers;
+	private List<String> args;
 		
     @Produce(uri = "direct:exec-in")
     private ProducerTemplate in;
+    
+    @Produce(uri = "seda:exec-bean-in?concurrentConsumers=10")
+    private ProducerTemplate exec_bean_in;
 
     @EndpointInject(uri = "mock:exec-out")
     private MockEndpoint out;
@@ -52,6 +58,8 @@ public class ExecTest extends CamelSpringTestSupport {
 	@Before
 	public void setUp() throws Exception {
 		super.setUp();
+		headers = new HashMap<String,Object>();
+		args = new ArrayList<String>();
 	}
 
 	@After
@@ -59,19 +67,20 @@ public class ExecTest extends CamelSpringTestSupport {
 		super.tearDown();
 	}
 	
+	private void sendExec(ProducerTemplate in) {
+		headers.put(EXEC_COMMAND_ARGS,args);
+		in.sendBodyAndHeaders("",headers);
+	}
+	@Ignore
 	@Test
-	public void testProducerExec() throws InterruptedException, IOException {
+	public void testSimpleExec() throws InterruptedException, IOException {
 		String echoString = "foobar";
 		out.setExpectedMessageCount(1);
-		Map<String,Object> headers = new HashMap<String,Object>();
-		List<String> args = new ArrayList<String>();
 		args.add("-n");
 		args.add(echoString);
 		
 		headers.put(EXEC_COMMAND_EXECUTABLE,"echo");
-		headers.put(EXEC_COMMAND_ARGS,args);
-		
-		in.sendBodyAndHeaders("",headers);
+		sendExec(in);
 
 		out.assertIsSatisfied();
 		List<Exchange> exchanges = out.getExchanges();
@@ -82,6 +91,77 @@ public class ExecTest extends CamelSpringTestSupport {
 			assertEquals("output is not equal",echoString,body);
 		}
 	}
+	@Ignore
+	@Test
+	public void testSimpleExecBean() throws InterruptedException, IOException {
+		String echoString = "foobar";
+		out.setExpectedMessageCount(1);
+		args.add("-n");
+		args.add(echoString);
+		
+		headers.put(EXEC_COMMAND_EXECUTABLE,"echo");
+		sendExec(in);
+
+		out.assertIsSatisfied();
+		List<Exchange> exchanges = out.getExchanges();
+		
+		for(Exchange exchange : exchanges) {
+			Message message = exchange.getIn();
+			String body = message.getBody(String.class);
+			assertEquals("output is not equal",echoString,body);
+		}
+	}
+	
+	@Ignore
+	@Test
+	public void testMetricExec() throws InterruptedException, IOException {
+		out.setExpectedMessageCount(1);
+		args.add("192.168.178.101");
+		args.add("jdg_sample");
+		args.add("1000");
+		
+		headers.put(EXEC_COMMAND_EXECUTABLE,"metric-add");
+
+		sendExec(in);
+
+		out.assertIsSatisfied();
+		List<Exchange> exchanges = out.getExchanges();
+		
+		for(Exchange exchange : exchanges) {
+			Message message = exchange.getIn();
+			String body = message.getBody(String.class);
+			//assertEquals("output is not equal",body);
+			LOG.info(body);
+		}
+	}
+	@Ignore
+	@Test
+	public void testMetricExecBean() throws InterruptedException, IOException {
+		Random random = new Random(1024L);
+
+		out.setExpectedMessageCount(100);
+		headers.put(EXEC_COMMAND_EXECUTABLE,"metric-add");
+
+		for (int i = 100 ; i != 0 ; i--) {
+			args.clear();
+			args.add("192.168.178.101");
+			args.add("jdg_sample");
+			args.add(Integer.toString(random.nextInt(1000)));
+			sendExec(in);
+			Thread.sleep(100);
+		}
+
+		out.assertIsSatisfied();
+		List<Exchange> exchanges = out.getExchanges();
+		
+		for(Exchange exchange : exchanges) {
+			Message message = exchange.getIn();
+			String body = message.getBody(String.class);
+			//assertEquals("output is not equal",body);
+			LOG.info(body);
+		}
+	}
+
 
 	@Override
 	protected AbstractApplicationContext createApplicationContext() {
