559c4a40f3efa90419a75e7ac7266a869c562966
jdgwartney <davidg@boundary.com>
2014-07-28 22:55:36 -0700

    Conversion of SNMP XML to SNMPEntry

---
+++ src/main/java/com/boundary/sdk/event/snmp/SNMPEntry.java
@@ -0,0 +1,28 @@
+package com.boundary.sdk.event.snmp;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlRootElement(name="entry")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class SNMPEntry {
+	@XmlElement
+    private String oid;
+	@XmlElement
+    private String value;
+
+	public String getOid() {
+		return oid;
+	}
+	public void setOid(String oid) {
+		this.oid = oid;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
