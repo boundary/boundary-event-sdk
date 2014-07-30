352e9e5a5d65e87f6e834a41e38508b986ac6b78
jdgwartney <davidg@boundary.com>
2014-07-30 12:55:34 -0700

    Added lookup of the metric name from the OID/name

--- src/main/java/com/boundary/sdk/event/snmp/SnmpToExecProcessor.java
+++ src/main/java/com/boundary/sdk/event/snmp/SnmpToExecProcessor.java
@@ -1,22 +1,35 @@
-/**
- * 
- */
+// Copyright 2014 Boundary, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.boundary.sdk.event.snmp;
 
 import static org.apache.camel.component.exec.ExecBinding.*;
 import static com.boundary.sdk.event.snmp.SnmpPollerRouteBuilder.BOUNDARY_HOSTNAME;
+import static com.boundary.sdk.event.util.PropertyUtils.*;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Vector;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.component.snmp.SnmpMessage;
+import org.apache.camel.component.syslog.SyslogSeverity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.snmp4j.PDU;
@@ -29,23 +42,12 @@ import com.boundary.sdk.event.RawEvent;
 import com.boundary.sdk.event.Severity;
 import com.boundary.sdk.event.Status;
 
-
-
-/**
- * Implements {@link Processor} interface and is responsible for converting
- * a {@link PDU} to a {@link RawEvent}.
- * 
- * @author davidg
- * 
- */
 public class SnmpToExecProcessor implements Processor {
-	
 	
 	private String command = "metric-add";
 	
     private static final Logger LOG = LoggerFactory.getLogger(SnmpToExecProcessor.class);
 
-	
 	/**
 	 * Handles the translation of a {@link snmp} to headers needed to run
 	 * Camel exec
@@ -63,7 +65,8 @@ public class SnmpToExecProcessor implements Processor {
 		args.add(message.getHeader(BOUNDARY_HOSTNAME).toString());
 			
 		// Add the metric name
-		args.add("jdg_sample");
+		String metricName = getMetricName(entry.getOid());
+		args.add(metricName);
 
 		// Add the measure
 		args.add(entry.getValue());
@@ -73,4 +76,12 @@ public class SnmpToExecProcessor implements Processor {
 
 		message.setBody("");
 	}
+	
+	private static Properties metricNameMap = new Properties();
+	
+	protected static String getMetricName(String severity) {
+		getProperties(metricNameMap,"snmp.metric.names.properties");
+		return metricNameMap.getProperty(severity.toString(),"UNKNOWN");
+	}
+
 }
