4be0970d1fb8bd23422b2791257392afe1c6b4fd
jdgwartney <davidg@boundary.com>
2015-01-26 21:17:56 -0800

    Change package

--- src/main/java/com/boundary/sdk/event/app/SyslogEventAdapter.java
+++ src/main/java/com/boundary/sdk/event/app/SyslogEventAdapter.java
@@ -11,10 +11,12 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package com.boundary.sdk.event;
+package com.boundary.sdk.event.app;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.boundary.sdk.event.CamelApplication;
 
 public class SyslogEventAdapter extends CamelApplication {
 	
