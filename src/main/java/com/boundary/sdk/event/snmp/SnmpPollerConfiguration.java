3c3fdeeef65a708f69fa6f2feaf31e6ea037e070
jdgwartney <davidg@boundary.com>
2015-01-29 11:43:03 -0800

    Add stub SNMP Poller Configuration and tests

---
+++ src/main/java/com/boundary/sdk/event/snmp/SnmpPollerConfiguration.java
@@ -0,0 +1,73 @@
+// Copyright 2014-2015 Boundary, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.boundary.sdk.event.snmp;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class SnmpPollerConfiguration {
+	
+	
+	private Set<String> oids;
+	private String community;
+	private long delay;
+	
+	public SnmpPollerConfiguration() {
+		oids = new LinkedHashSet<String>();
+	}
+	
+	public void addOid(String oid) {
+		oids.add(oid);
+	}
+	
+	public String getOidsAsString() {
+		StringBuilder builder = new StringBuilder();
+		
+		boolean first = true;
+		for (String oid: oids) {
+			if (first == false) {
+				builder.append(",");
+			}
+			builder.append(oid);
+			first = false;
+		}
+		
+		return builder.toString();
+	}
+	
+	public Set<String> getOids() {
+		return this.oids;
+	}
+
+	public String getCommunity() {
+		return community;
+	}
+
+	public void setCommunity(String community) {
+		this.community = community;
+	}
+
+	public long getDelay() {
+		return delay;
+	}
+
+	public void setDelay(long delay) {
+		this.delay = delay;
+	}
+	
+}
---
+++ src/test/java/com/boundary/sdk/event/snmp/SnmpPollerConfigurationTest.java
@@ -0,0 +1,100 @@
+// Copyright 2014-2015 Boundary, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.boundary.sdk.event.snmp;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SnmpPollerConfigurationTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSnmpPollerConfiguration() {
+		SnmpPollerConfigurationTest config = new SnmpPollerConfigurationTest();
+	}
+
+	@Test
+	public void testAddOid() {
+		SnmpPollerConfiguration config = new SnmpPollerConfiguration();
+		config.addOid("1.3.6.1.2.1.25.1.5.0");
+	}
+
+	@Test
+	public void testGetOidsSize() {
+		SnmpPollerConfiguration config = new SnmpPollerConfiguration();
+		config.addOid("1.3.6.1.2.1.25.1.5.0");
+		config.addOid("1.3.6.1.2.1.25.1.5.0");
+		
+		Set<String> set = config.getOids();
+		assertEquals("check set count",1,set.size());
+		assertEquals("check getOidsAsString","1.3.6.1.2.1.25.1.5.0",config.getOidsAsString());
+	}
+	
+	@Test
+	public void testDuplicateOids() {
+		String expectedOidString = "1.3.6.1.2.1.25.1.5.0,1.3.6.1.2.1.25.1.6.0,1.3.6.1.2.1.6.9.0,1.3.6.1.2.1.7.1.0,1.3.6.1.2.1.7.4.0,1.3.6.1.2.1.6.10.0,1.3.6.1.2.1.6.11.0,1.3.6.1.2.1.4.3.0,1.3.6.1.2.1.4.10.0";
+		SnmpPollerConfiguration config = new SnmpPollerConfiguration();
+		config.addOid("1.3.6.1.2.1.25.1.5.0");
+		config.addOid("1.3.6.1.2.1.25.1.5.0");
+
+		config.addOid("1.3.6.1.2.1.25.1.6.0");
+		config.addOid("1.3.6.1.2.1.6.9.0");
+		config.addOid("1.3.6.1.2.1.7.1.0");
+		config.addOid("1.3.6.1.2.1.7.4.0");
+		config.addOid("1.3.6.1.2.1.6.10.0");
+		config.addOid("1.3.6.1.2.1.6.11.0");
+		config.addOid("1.3.6.1.2.1.4.3.0");
+		config.addOid("1.3.6.1.2.1.4.10.0");
+
+		Set<String> set = config.getOids();
+		assertEquals("check set count",9,set.size());
+		assertEquals("check getOidsAsString",expectedOidString,config.getOidsAsString());
+	}
+
+	@Test
+	public void testCommunity() {
+		SnmpPollerConfiguration config = new SnmpPollerConfiguration();
+		config.setCommunity("foobar");
+		assertEquals("check community","foobar",config.getCommunity());
+	}
+	
+	@Test
+	public void testDelay() {
+		SnmpPollerConfiguration config = new SnmpPollerConfiguration();
+		config.setDelay(30);
+		assertEquals("check delay",30,config.getDelay());
+	}
+}
