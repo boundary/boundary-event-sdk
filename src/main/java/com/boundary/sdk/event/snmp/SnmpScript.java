a01933aeeac4bf8d5477c5e9880449d452d7f3b7
jdgwartney <davidg@boundary.com>
2015-02-19 16:24:20 -0800

    Add wrapper class for getting access to the SmiManager from SNMP4J

---
+++ src/main/java/com/boundary/sdk/event/snmp/SnmpScript.java
@@ -0,0 +1,41 @@
+// Copyright 2014-2015 Boundary, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package com.boundary.sdk.event.snmp;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.boundary.sdk.event.BoundaryEventRouteBuilder;
+import com.snmp4j.smi.SmiManager;
+
+/**
+ * JavaScript interface to SMI Manager of SNMP4j
+ */
+public class SnmpScript extends SmiSupport {
+	
+	private static Logger LOG = LoggerFactory.getLogger(BoundaryEventRouteBuilder.class);
+	
+	public SnmpScript() {
+	}
+	
+	public void load() {
+		String license = System.getenv("BOUNDARY_MIB_LICENSE");
+		String repositoryPath = System.getenv("BOUNDARY_MIB_REPOSITORY");
+		LOG.info("MIB repository path: {}",repositoryPath);
+		this.setLicense(license);
+		this.setRepository(repositoryPath);
+		this.initialize();
+		this.loadModules();
+	}
+}
